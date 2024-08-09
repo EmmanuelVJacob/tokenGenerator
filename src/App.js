409783a9b1fb934ee2103bfccfab52e100a79830
Emmanuel Jacob <emmanueljacob678@gmail.com>
2024-08-09 10:28:36 +0530

    Heading created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Heading from "./Components/Heading";
 
 function App() {
   return (
     <div className="App">
-     {/* //generator token */}
+      <Heading />
     </div>
   );
 }
---
+++ src/Components/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Heading = () => {
+  return (
+    <div className="flex items-center justify-center  bg-gradient-to-r from-blue-500 to-purple-600 p-4">
+      <h1 className="text-3xl sm:text-3xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold text-white drop-shadow-lg text-center">
+        Token Generator
+      </h1>
+    </div>
+  );
+};
+
+export default Heading;
